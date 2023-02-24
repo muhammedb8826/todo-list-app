61ca01c2a0a923c929df4aae86e125751f80d1bb
Muhammed Berisso <muhammedb8826@gmail.com>
2023-02-24 23:52:43 +0300

    Fix length of the array

--- src/modules/createData.js
+++ src/modules/createData.js
@@ -1,6 +1,6 @@
 const userInput = document.getElementById('todoInput');
 const createData = (todoCollection) => {
-  const todo = { description: userInput.value, completed: false, index: todoCollection.length + 1 };
+  const todo = { description: userInput.value, completed: false, index: todoCollection.length };
   todoCollection.push(todo);
   userInput.value = '';
   userInput.focus();
