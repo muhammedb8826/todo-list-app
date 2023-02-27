57ac73226b455d39f8174a8701d28cd0295c87a6
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-02-27 16:41:24 +0300

    working with icons

--- src/modules/editData.js
+++ src/modules/editData.js
@@ -3,37 +3,31 @@ import saveToLocalStorage from './saveToDb.js';
 const editData = (dataArray) => {
   const todoLists = document.querySelectorAll('.list');
   const todoContent = document.querySelectorAll('.todo-content');
-  const dragIcon = document.querySelectorAll('.drag');
-  const trashIcon = document.querySelectorAll('.trash');
+  const dragIcon = document.querySelectorAll('.drag-icon');
+  const trashIcon = document.querySelectorAll('.trash-icon');
 
-  todoContent.forEach((todo, i) => todo.addEventListener('click', () => {
-    todo.focus();
-    todoLists[i].classList.add('active');
-    trashIcon[i].classList.add('active');
-    dragIcon[i].style.display = 'none';
-  }));
+todoLists.forEach(list=>list.addEventListener('click',()=>{
+  console.log('I am list item');
+ 
+}))
 
-  todoContent.forEach((todo, i) => todo.addEventListener('focusout', () => {
-    dataArray[i].description = todoContent[i].value;
-    trashIcon[i].style.display = 'none';
-    dragIcon[i].style.display = 'flex';
-    todoLists[i].classList.remove('active');
-    saveToLocalStorage(dataArray);
-  }));
+todoContent.forEach((input,i)=>input.addEventListener('click',(e)=>{
+  console.log('I am input');
+  e.stopImmediatePropagation();
+  todoLists[i].classList.add('active')
+  trashIcon[i].classList.add('active');
+  dragIcon[i].style.display = 'none';
+  trashIcon.forEach((trash)=>trash.addEventListener('click',()=>{
+    console.log('trash clicked');
+  }))
+}))
 
-  todoContent.forEach((todo, i) => todo.addEventListener('keypress', (e) => {
-    if (e.key === 'Enter') {
-      dataArray[i].description = todoContent[i].value;
-      trashIcon[i].style.display = 'none';
-      todoLists[i].classList.remove('active');
-      saveToLocalStorage(dataArray);
-      todoContent[i].blur();
-    }
-  }));
-  todoLists.forEach((el) => el.addEventListener('click', () => {
-    dragIcon.forEach((drag) => drag.addEventListener('click', (e) => {
-      e.stopPropagation();
-    }));
-  }));
+// todoContent.forEach((input,i)=>input.addEventListener('focusout',(e)=>{
+//   e.stopImmediatePropagation();
+//   trashIcon[i].classList.remove('active');
+//   dragIcon[i].style.display = 'flex';
+// }))
+
+ 
 };
 export default editData;
