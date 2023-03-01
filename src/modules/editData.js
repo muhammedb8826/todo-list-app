29ff3979c3ce35d3c76a459718718e5486eb842a
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-03-01 12:09:19 +0300

    Fixed trash icon

--- src/modules/editData.js
+++ src/modules/editData.js
@@ -11,13 +11,19 @@ const editData = (dataArray) => {
   const dragIcon = document.querySelectorAll('.drag-icon');
   const trashIcon = document.querySelectorAll('.trash-icon');
 
-  todoContent.forEach((input, i) => input.addEventListener('click', (e) => {
+  todoLists.forEach((input, item) => input.addEventListener('click', (e) => {
     e.stopImmediatePropagation();
-    todoLists[i].classList.add('active');
-    todoLists[i].focus();
-    trashIcon[i].style.display = 'flex';
-    dragIcon[i].style.display = 'none';
-    todoContent[i].focus();
+    todoLists[item].classList.add('active');
+    trashIcon[item].style.display = 'flex';
+    dragIcon[item].style.display = 'none';
+    todoContent[item].focus();
+    for (let i = 0; i < todoLists.length; i += 1) {
+      if (item !== i) {
+        todoLists[i].classList.remove('active');
+        trashIcon[i].style.display = 'none';
+        dragIcon[i].style.display = 'block';
+      }
+    }
   }));
 
   todoContent.forEach((todo, i) => todo.addEventListener('keypress', (e) => {
@@ -29,13 +35,6 @@ const editData = (dataArray) => {
       trashIcon[i].style.display = 'none';
       dragIcon[i].style.display = 'block';
     }
-  }));
-
-  todoLists.forEach((trash, i) => trash.addEventListener('mouseleave', () => {
-    todoContent[i].blur();
-    todoLists[i].classList.remove('active');
-    trashIcon[i].style.display = 'none';
-    dragIcon[i].style.display = 'block';
   }));
 };
 export default editData;
