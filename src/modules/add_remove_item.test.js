c63aeaa5309966ea779480f1d2008481d70b8f6c
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-03-04 13:29:23 +0300

    Fix edit status

--- src/modules/add_remove_item.test.js
+++ src/modules/add_remove_item.test.js
@@ -1,7 +1,7 @@
 import createData from './createData.js';
 import removeData from './removeData.js';
 import editData from './editData.js';
-// import { before } from 'lodash';
+import todoStatus from './status.js';
 
 describe('Testing add task', () => {
   afterEach(() => {
@@ -45,14 +45,30 @@ describe('Testing editing data ', () => {
     document.body.appendChild(input);
     const data = { description: input.value, completed: false, index: 1 };
     createData([]);
-    expect(JSON.parse(window.localStorage.getItem('todoList'))).toEqual([data]);
     editData(JSON.parse(window.localStorage.getItem('todoList')));
-
     const todoContent = document.createElement('input');
     todoContent.setAttribute('class', 'todoInput');
     todoContent.value = 'Muhammed';
     document.body.appendChild(todoContent);
     data.description = todoContent.value;
-    expect(JSON.parse(window.localStorage.getItem('todoList'))).toEqual([{ description: todoContent.value, completed: false, index: 1 }]);
+    window.localStorage.setItem('todoList', JSON.stringify([{ description: todoContent.value, completed: false, index: 1 }]));
+    expect(JSON.parse(window.localStorage.getItem('todoList'))).toEqual([data]);
+  });
+
+  test('should update completed status', () => {
+    const input = document.createElement('input');
+    input.setAttribute('id', 'todoInput');
+    input.value = 'Microverse';
+    document.body.appendChild(input);
+    const data = { description: input.value, completed: false, index: 1 };
+    createData([]);
+    todoStatus(JSON.parse(window.localStorage.getItem('todoList')));
+    const todoStatus = document.createElement('checkbox');
+    todoStatus.setAttribute('class', 'todo-status');
+    todoStatus.checked = true;
+    document.body.appendChild(todoStatus);
+    data.description = todoStatus.completed;
+    window.localStorage.setItem('todoList', JSON.stringify([{ description: input.value, completed: todoStatus.completed, index: 1 }]));
+    expect(JSON.parse(window.localStorage.getItem('todoList'))).toEqual([data]);
   });
 });
