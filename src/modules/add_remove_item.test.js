70392cd3152dd587c9e76130a00014264df5b1bf
Muhammed Berisso <115731485+muhammedb8826@users.noreply.github.com>
2023-03-04 12:54:35 +0300

    Add editing test

--- src/modules/add_remove_item.test.js
+++ src/modules/add_remove_item.test.js
@@ -1,5 +1,7 @@
 import createData from './createData.js';
 import removeData from './removeData.js';
+import editData from './editData.js';
+// import { before } from 'lodash';
 
 describe('Testing add task', () => {
   afterEach(() => {
@@ -11,7 +13,7 @@ describe('Testing add task', () => {
     input.setAttribute('id', 'todoInput');
     input.value = 'Microverse';
     document.body.appendChild(input);
-    const data = { description: 'Microverse', completed: false, index: 1 };
+    const data = { description: input.value, completed: false, index: 1 };
     createData([]);
     expect(JSON.parse(window.localStorage.getItem('todoList'))).toEqual([data]);
   });
@@ -21,11 +23,36 @@ describe('Testing add task', () => {
     input.setAttribute('id', 'todoInput');
     input.value = 'Microverse';
     document.body.appendChild(input);
-    const data = { description: 'Microverse', completed: false, index: 1 };
+    const data = { description: input.value, completed: false, index: 1 };
     createData([]);
     expect(JSON.parse(window.localStorage.getItem('todoList'))).toEqual([data]);
     removeData(JSON.parse(window.localStorage.getItem('todoList')), data.index - 1);
 
     expect(JSON.parse(window.localStorage.getItem('todoList'))).toEqual([]);
+  });
+});
+
+describe('Testing editing data ', () => {
+  afterEach(() => {
+    document.body.innerHTML = '';
+    window.localStorage.clear();
+  });
+
+  test('should update edited data', () => {
+    const input = document.createElement('input');
+    input.setAttribute('id', 'todoInput');
+    input.value = 'Microverse';
+    document.body.appendChild(input);
+    const data = { description: input.value, completed: false, index: 1 };
+    createData([]);
+    expect(JSON.parse(window.localStorage.getItem('todoList'))).toEqual([data]);
+    editData(JSON.parse(window.localStorage.getItem('todoList')));
+
+    const todoContent = document.createElement('input');
+    todoContent.setAttribute('class', 'todoInput');
+    todoContent.value = 'Muhammed';
+    document.body.appendChild(todoContent);
+    data.description = todoContent.value;
+    expect(JSON.parse(window.localStorage.getItem('todoList'))).toEqual([{ description: todoContent.value, completed: false, index: 1 }]);
   });
 });
